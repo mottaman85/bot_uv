ada0c835486294d81f607f9ec786b2e54bd0735b
Paulino Mota hernandez <paulinomotahernandez@Paulinos-MacBook-Pro-2.local>
2019-07-02 18:10:30 -0500

    action

--- bot.js
+++ bot.js
@@ -1,14 +1,27 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-const { ActivityHandler } = require('botbuilder');
+const { ActivityHandler, MessageFactory } = require('botbuilder');
 
 class MyBot extends ActivityHandler {
     constructor() {
         super();
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         this.onMessage(async (context, next) => {
-            await context.sendActivity(`You said '${ context.activity.text }'`);
+
+
+            if(context.activity.text.toUpperCase() === "HOLA"){
+                return await context.sendActivity(`HOLA AMIGO`);    
+            }if(context.activity.text.toUpperCase() === "MENU"){
+                return await context.sendActivity(`EL MENU ES EL SIGUIENTE`);    
+            }if(context.activity.text.toUpperCase() === "FAQ"){
+                return await context.sendActivity(`MENU DE PREGUNTAS FRECUENTES`);    
+            }else{
+                var reply = MessageFactory.suggestedActions(['HOLA', 'MENU', 'FAQ'], 'Hola, AUN ESTOY APRENDIENDO, PERO PUEDO HACER LO SIGUIENTE');
+                return await context.sendActivity(reply);       
+            }
+
+            
             // By calling next() you ensure that the next BotHandler is run.
             await next();
         });
