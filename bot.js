34285c67ac62d5ba739d5c96e9d0f7281629bb97
Paulino Mota hernandez <paulinomotahernandez@Paulinos-MacBook-Pro-2.local>
2019-07-02 18:21:16 -0500

    se agrega hero card

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-const { ActivityHandler, MessageFactory } = require('botbuilder');
+const { ActivityHandler, MessageFactory, CardFactory } = require('botbuilder');
 
 class MyBot extends ActivityHandler {
     constructor() {
@@ -15,7 +15,21 @@ class MyBot extends ActivityHandler {
             }if(context.activity.text.toUpperCase() === "MENU"){
                 return await context.sendActivity(`EL MENU ES EL SIGUIENTE`);    
             }if(context.activity.text.toUpperCase() === "FAQ"){
-                return await context.sendActivity(`MENU DE PREGUNTAS FRECUENTES`);    
+                context.sendActivity(
+                    { attachments: [
+                        CardFactory.videoCard(
+            '2018 Imagine Cup World Championship Intro',
+            [{ url: 'https://sec.ch9.ms/ch9/783d/d57287a5-185f-4df9-aa08-fcab699a783d/IC18WorldChampionshipIntro2.mp4' }],
+            [{
+                type: 'openUrl',
+                title: 'Lean More',
+                value: 'https://channel9.msdn.com/Events/Imagine-Cup/World-Finals-2018/2018-Imagine-Cup-World-Championship-Intro'
+            }],
+            {
+                subtitle: 'by Microsoft',
+                text: 'Microsoft\'s Imagine Cup has empowered student developers around the world to create and innovate on the world stage for the past 16 years. These innovations will shape how we live, work and play.'
+            }
+        )] })
             }else{
                 var reply = MessageFactory.suggestedActions(['HOLA', 'MENU', 'FAQ'], 'Hola, AUN ESTOY APRENDIENDO, PERO PUEDO HACER LO SIGUIENTE');
                 return await context.sendActivity(reply);       
